6c1f32351c8d5c3ecbab8d03cdf178ba1dd508d8
Tobias <kruseborn@gmail.com>
2021-12-06 09:31:46 +0100

    boids

--- src/scenes/boids/boids_main.cpp
+++ src/scenes/boids/boids_main.cpp
@@ -7,6 +7,15 @@
 
 int main() {
 
+  glm::vec3 u{3, -4, 1};
+  glm::vec3 v{0, 2, -3};
+  glm::vec3 w{3, 2, 2};
+
+  glm::vec3 ww = glm::cross(v, w);
+  float s = glm::dot(ww, ww);
+  float ss = ww.x + ww.y + ww.z;
+
+
   mg::initWindow(1024, 960);
   initScene();
   while (mg::startFrame()) {
